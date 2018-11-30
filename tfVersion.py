ba3e79bcf7a974c534ad865ec0c413453c94cee4
cruobo <cruobo@126.com>
2018-11-30 18:52:48 +0800

    first commit

---
+++ tfVersion.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 16 12:40:03 2018
+mnist数据集-手写识别简单示例-使用dropout方法
+Huaqiao University 
+@author: lopo
+"""
+import tensorflow as tf
+
+print(tf.__version__)
